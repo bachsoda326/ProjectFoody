8b0a3f6f69a933ac8de861eaeb33fda0dda00c62
DESKTOP-J2U6JA8\Chelsea _198 <phantrongba1998@gmail.com>
2019-02-13 02:33:49 +0700

    Create_Project

---
+++ AppFoody/app/src/main/java/com/example/appfoody/View/View/View/TrangChuActivity.java
@@ -0,0 +1,15 @@
+package com.example.appfoody.View.View.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.appfoody.R;
+
+public class TrangChuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_trangchu);
+    }
+}
